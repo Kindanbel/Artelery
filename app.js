7a6eb0e938e9b9f53a5910e0f6a2ff96acfdc96b
Abel Daniel <abelonah@yahoo.com>
2024-02-29 00:50:09 +0100

    Responsive and animation job

--- app.js
+++ app.js
@@ -23,23 +23,21 @@ scrollContainer.addEventListener("wheel", (evt)=>{
     scrollContainer.scrollLeft += evt.deltaX;
     scrollContainer.style.scrollBehavior = "auto";
     bckBtn.style.opacity = '1';
-})
+ })
 
 nxtBtn.addEventListener("click", ()=>{
     scrollContainer.style.scrollBehavior = "smooth";
-    scrollContainer.scrollLeft += 1500;
+    scrollContainer.scrollLeft += 3000;
     bckBtn.style.opacity = '1';
     nxtBtn.style.opacity = '0';
 })
 
 bckBtn.addEventListener("click", ()=>{
     scrollContainer.style.scrollBehavior = "smooth";
-    scrollContainer.scrollLeft -= 1500;
+    scrollContainer.scrollLeft -= 3000;
     bckBtn.style.opacity = '0';
     nxtBtn.style.opacity = '1';
 })
-
-
 
 
 //Making responsive buttons
@@ -55,3 +53,73 @@ sec4Btn.forEach((btn)=>{
         btn.style.background = "#09adb5"
     })
 })
+
+
+//Making Hamburger menu
+
+let isMenuOpen = false;
+
+const menuBtn = document.querySelector(".menu-btn");
+
+const navLinks = document.querySelector(".list-items");
+
+menuBtn.addEventListener("click", ()=>{
+    isMenuOpen = !isMenuOpen;
+
+    if(isMenuOpen){
+        menuBtn.classList.add("open");
+    } else{
+        menuBtn.classList.remove("open");
+    };
+
+    navLinks.classList.toggle("active");
+});
+
+
+//Text rotation animation
+
+
+"use strict";
+
+let words = document.querySelectorAll(".word");
+
+words.forEach((word)=>{
+    let letters = word.textContent.split("");
+    word.textContent = "";
+    letters.forEach((letter)=>{
+        let span = document.createElement("span");
+        span.textContent = letter;
+        span.className = "letter";
+        word.append(span);
+    });
+});
+
+let currentWordIndex = 0;
+let maxWordIndex = words.length - 1;
+words[currentWordIndex].style.opacity = "1";
+
+let rotateText = ()=>{
+    let currentWord = words[currentWordIndex];
+    let nextWord = currentWordIndex === maxWordIndex ? words[0] : words[currentWordIndex + 1];
+    
+    //rotate out letters of current word
+    Array.from(currentWord.children).forEach((letter, i)=>{
+        setTimeout(()=>{
+            letter.className = "letter out";
+        }, i * 80);
+    });
+
+    //reaveal and rotate next word
+    nextWord.style.opacity = "1";
+    Array.from(nextWord.children).forEach((letter, i)=>{
+        letter.className = "letter behind";
+        setTimeout(()=>{
+            letter.className = "letter in";
+        }, 340 + i * 80 );
+    });
+   currentWordIndex = currentWordIndex === maxWordIndex ? 0 : currentWordIndex + 1;
+};
+
+rotateText();
+setInterval(rotateText, 4000);
+
